e78344f27bcade71817bea53ac6b933a79118ca8
Elymion <woosuk7377@gmail.com>
2022-07-02 01:55:06 +0900

    Revert "DS-Shop re"
    This reverts commit 8d4fe6dd054ec2e1c759dfb20bbafef828b723a1.

--- DS-Shop/src/main/java/kr/dreamstory/shop/shop/ShopGUI.kt
+++ DS-Shop/src/main/java/kr/dreamstory/shop/shop/ShopGUI.kt
@@ -1,11 +1,15 @@
 package kr.dreamstory.shop.shop
 
-import kr.dreamstory.library.gui.DSGUI
+import com.dreamstory.library.gui.DSGUI
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.event.inventory.InventoryDragEvent
 
 class ShopGUI(private val shopData: ShopData): DSGUI(shopData.size, shopData.title) {
+
+    init {
+
+    }
 
     override fun init() {}
 
--- DS-Shop/src/main/java/kr/dreamstory/shop/shop/ShopIcon.kt
+++ DS-Shop/src/main/java/kr/dreamstory/shop/shop/ShopIcon.kt
@@ -1,14 +1,14 @@
 package kr.dreamstory.shop.shop
 
-import kr.dreamstory.library.item.dreamstory.data.DSItemManager
+import kr.dreamstory.library.item.dreamstory.data.DSItemDataManager
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
 class ShopIcon(private val itemKey: String, val slot: Int) {
 
     fun getItem(): ItemStack {
-        val dsItem = DSItemManager.getDSItem(itemKey)
-            ?: DSItemManager.getNewDSItem("Null", ItemStack(Material.RABBIT_HIDE),false)
+        val dsItem = DSItemDataManager.getDSItem(itemKey)
+            ?: DSItemDataManager.getNewDSItem("Null", ItemStack(Material.RABBIT_HIDE),false)
         return dsItem.itemStack
     }
 
