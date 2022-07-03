3f1d016901f9ca403fb50663d69bc990dade4a8b
BuildTools <unconfigured@null.spigotmc.org>
2022-07-04 01:14:56 +0900

    add Economy add PlayerData

--- DS-Ability/build.gradle.kts
+++ DS-Ability/build.gradle.kts
@@ -4,9 +4,13 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://maven.enginehub.org/repo/")
+    maven("https://mvn.lumine.io/repository/maven-public/")
+    maven("https://repo.dmulloy2.net/repository/public/")
 }
 
 dependencies {
     implementation(project(":DS-Library"))
+    compileOnly("com.comphenix.protocol","ProtocolLib","4.8.0")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.1.0-SNAPSHOT")
+    compileOnly("io.lumine:Mythic-Dist:5.0.3")
 }
