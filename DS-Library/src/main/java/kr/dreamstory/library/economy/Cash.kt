eb9f97cec4adb115c4ff5c16d3db2ff8b03f0ae8
BuildTools <unconfigured@null.spigotmc.org>
2022-07-05 22:23:11 +0900

    add economy and ability

--- DS-Library/src/main/java/kr/dreamstory/library/economy/Cash.kt
+++ DS-Library/src/main/java/kr/dreamstory/library/economy/Cash.kt
@@ -6,14 +6,14 @@ class Cash(cash: Long = 0) {
     var amount = cash
         private set
 
-    fun set(amount: Long) {
-        this.amount = amount
-    }
-    fun add(amount: Long) {
-        this.amount += amount
-    }
-    fun remove(amount: Long) {
+    fun set(amount: Long) { this.amount = amount }
+    fun add(amount: Long) { this.amount += amount }
+    fun remove(amount: Long) { this.amount -= amount }
+
+    fun tryRemove(amount: Long): Boolean {
+        if(this.amount < amount) return false
         this.amount -= amount
+        return true
     }
 
     fun hasAmount(amount: Long): Boolean {
--- DS-Library/src/main/java/kr/dreamstory/library/economy/Money.kt
+++ DS-Library/src/main/java/kr/dreamstory/library/economy/Money.kt
@@ -6,14 +6,14 @@ class Money(money: Long = 0) {
     var amount = money
         private set
 
-    fun set(amount: Long) {
-        this.amount = amount
-    }
-    fun add(amount: Long) {
-        this.amount += amount
-    }
-    fun remove(amount: Long) {
+    fun set(amount: Long) { this.amount = amount }
+    fun add(amount: Long) { this.amount += amount }
+    fun remove(amount: Long) { this.amount -= amount }
+
+    fun tryRemove(amount: Long): Boolean {
+        if(this.amount < amount) return false
         this.amount -= amount
+        return true
     }
 
     fun hasAmount(amount: Long): Boolean {
