68cb318ac6f49ba8f1190d12328d0ba81d0b4805
BuildTools <unconfigured@null.spigotmc.org>
2022-07-23 22:52:49 +0900

    .

---
+++ DS-DataLoader/build.gradle.kts
@@ -0,0 +1,13 @@
+group = "kr.dreamstory.data_loader"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":DS-Library"))
+    implementation(project(":DS-Ability"))
+    implementation(project(":DS-Community"))
+
+}
